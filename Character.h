fa64f9de3feb25205afc6fe3c285e7c2002c2db9
Alex Bahna <alexbahna@gmail.com>
2016-11-08 20:47:41 -0600

    Update Character.h

--- Character.h
+++ Character.h
@@ -1,5 +1,28 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
+//=========================================================//
+//                                                         //
+//          Last Modified By: Alex "Fuck" Bahna            //
+//                                                         //   
+//               -11-----8-----16-                         //
+//                                                         //
+//       Main character class for the user / player        //
+//                                                         //
+//=========================================================//
+
+
+class Character {
+    public:
+        double health;              //Life points of player
+        double points;              //Score for player
+        int keys;                   //Perhaps used for entering doorways
+        int pos_on_map;             //Pretty fucking obvious
+
+        Character();
+        ~Character();
+
+        //Feel free to add more
+};
 
 
 
