e8663ea36104b4d85daaa7fb2bd3c6df422c001a
Todd Selwitz <selwitz.todd@gmail.com>
2016-11-11 13:48:11 -0500

    Added an 'item' array and an integer to represent 'lives'.

--- Character.h
+++ Character.h
@@ -2,9 +2,9 @@
 #define CHARACTER_H
 //=========================================================//
 //                                                         //
-//          Last Modified By: Adriano Santos               //
+//          Last Modified By: Todd Selwitz                 //
 //                                                         //   
-//               -11-----8-----16-                         //
+//               -11-----11-----16-                        //
 //                                                         //
 //       Main character class for the user / player        //
 //                                                         //
@@ -15,12 +15,19 @@
 //ie blue key = 2. red key = 1. certain keys work on certain doors.
 //door can check key vector or set. im leaving vector for now
 
+//
 class Character {
     public:
+	int lives; 		    //Number of lives before you are sent to the beginning of the dungeon
         double health;              //Life points of player
-        double points;              //Score for player
+	double points;              //Score for player
         vector<int> keys;           //Perhaps used for entering doorways
-        int pos_on_map;             //Pretty fucking obvious
+	
+	int items[10];              //Idea: set item[i] to x if player has x of that item.
+				    //If item '0' is a health potion item[0] might have a
+				    //value of 3, for example
+        
+	int pos_on_map;             //Pretty fucking obvious
 
         Character();
         ~Character();
